5c7272c4dce61993d0e7c028435683b86123b7a7
Danny Eck <eck.danny@gmail.com>
2019-11-27 14:56:16 -0800

    wip

--- cli.js
+++ cli.js
@@ -12,16 +12,20 @@ const execA = require('execa')
 const conventionalRecommendedBump = require('conventional-recommended-bump')
 
 const recBump = promisify(conventionalRecommendedBump)
-const scriptName = basename(__filename)
+const scriptName = basename(__filename);
 
-if (process.env.npm_lifecycle_event !== scriptName) {
-	logger.error(scriptName, `only run \`${scriptName}\` as an npm lifecycle script please!`)
-	process.exit(1)
-}
+// @todo(de): this fails when used as a pkgJson#script ref!
+// if (process.env.npm_lifecycle_event !== scriptName) {
+// 	console.log(process.env)
+// 	logger.error(scriptName, `only run \`${scriptName}\` as an npm lifecycle script please!`)
+// 	process.exit(1)
+// }
 
 (async () => {
 	try {
-		const { recommendedBumpOpts } = await require('conventional-changelog-angular-bitbucket')
+		const {
+			recommendedBumpOpts
+		} = await require('conventional-changelog-angular-bitbucket')
 		// @todo(de): get `whatBump` from preset dynamically
 		const whatBumpAngularBitbucket = recommendedBumpOpts.whatBump
 		const whatBump = (...args) => {
@@ -45,15 +49,23 @@ if (process.env.npm_lifecycle_event !== scriptName) {
 		// @todo(de): get dynamically (from argv?)
 		await execA(getExecPath(), [
 			'conventional-changelog',
-			'-p', 'angular-bitbucket',
-			'-i', 'CHANGELOG.md',
+			'-p',
+			'angular-bitbucket',
+			'-i',
+			'CHANGELOG.md',
 			'-s'
 		])
 		await execA('git', ['add', 'CHANGELOG.md', 'package.json'])
 		await execA('git', ['commit', '-m', `chore(release): ${nextVersion}`])
 
 		// @todo(de): get this from npm config or from preset config
-		await execA('git', ['tag', '-a', '-m', `chore(release): ${nextVersion}`, `v${nextVersion}`])
+		await execA('git', [
+			'tag',
+			'-a',
+			'-m',
+			`chore(release): ${nextVersion}`,
+			`v${nextVersion}`
+		])
 	} catch (err) {
 		logger.error(scriptName, err.message)
 		process.exit(1)
@@ -63,10 +75,10 @@ if (process.env.npm_lifecycle_event !== scriptName) {
 function getExecPath() {
 	const execPath = basename(process.env.npm_execpath)
 	switch (true) {
-		case (execPath === 'npm-cli.js'): {
+		case execPath === 'npm-cli.js': {
 			return join(dirname(process.env.npm_execpath), 'npx-cli.js')
 		}
-		case (execPath === 'yarn.js'): {
+		case execPath === 'yarn.js': {
 			return process.env.npm_execpath
 		}
 		default: {
@@ -80,17 +92,28 @@ function warnIfNoUsefulCommits(result, ...args) {
 	const [commits] = args
 	const LEVELS = ['major', 'minor', 'patch']
 
+	console.log(result)
+	console.log(commits)
+
+	// @todo(de): don't neglect BREAKING CHANGE <= completely changes things
+
 	// @todo(de): how can this be retrieved programatically from preset?
 	const ACTION_TYPES = ['feat', 'fix', 'perf']
 
-	const meaningfulCommits = commits.filter(commit => ACTION_TYPES.includes(commit.type))
+	// e.g.; significant changes have occured
+	const isDefaultLevel = result.level === 2
 
-	if (meaningfulCommits.length) return
+	const meaningfulCommits = commits.filter(commit =>
+		ACTION_TYPES.includes(commit.type)
+	)
+
+	if (!isDefaultLevel || meaningfulCommits.length) return
 
 	const msgs = []
 
 	if (!commits.length) msgs.push('no commits since last release!')
-	if (commits.length && !meaningfulCommits.length) msgs.push('no meaningful commits since last release!')
+	if (commits.length && !meaningfulCommits.length)
+		msgs.push('no meaningful commits since last release!')
 
 	msgs.push('an empty changelog section will be geneated')
 	msgs.push(`using semver bump: ${chalk.yellow(LEVELS[result.level])}`)
